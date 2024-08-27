7de08328b2b3261be3676f8c2b8fbb5ea7ebc266
Michael Astfalk <mtastfalkjr@verizon.net>
2024-08-27 09:59:48 -0400

    Created submit.py
    Moved sending notifications to submit.py so that main.py can focus on handling user input.

--- src/main.py
+++ src/main.py
@@ -2,14 +2,13 @@
 # Sends notifcations to the desktop.
 # calicojack1720
 # Created: 8/26/24
-# Updated: 8/26/24
+# Updated: 8/27/24
 
-from notifypy import Notify
+# file -- main.py --
+
+from submit import send_notification
 
 notification_title = input("Enter the notification title: ")
 notification_message = input("Enter the notification message: ")
 
-notification = Notify()
-notification.title = notification_title
-notification.message = notification_message
-notification.send()
+send_notification(notification_title, notification_message)
