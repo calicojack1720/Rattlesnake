f3232df1fdc952ec867e9d4ebca4f8b68f0e393d
Michael Astfalk <mtastfalkjr@verizon.net>
2024-08-26 22:16:24 -0400

    Setup basic notification format

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
+# Rattlesnake
+# Sends notifcations to the desktop.
+# calicojack1720
+# Created: 8/26/24
+# Updated: 8/26/24
 
+from notifypy import Notify
+
+notification = Notify()
+notification.title = "Test"
+notification.message = "I am testing notifypy"
+notification.send()
